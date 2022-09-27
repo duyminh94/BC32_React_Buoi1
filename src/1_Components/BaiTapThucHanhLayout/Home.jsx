b6ec06b649ea357c1547baa5ebe389305c44c0e0
Le Nguyen Duy Minh <108447811+duyminh94@users.noreply.github.com>
2022-09-27 18:01:08 +0700

    uploadsource

---
+++ src/1_Components/BaiTapThucHanhLayout/Home.jsx
@@ -0,0 +1,19 @@
+import Banner from "./Banner";
+import Footer from "./Footer";
+import Header from "./Header";
+import Item from "./Item";
+import "./home.css"
+
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <Banner />
+            <Item />
+            <Footer />
+        </div>
+    )
+}
+
+export default Home
